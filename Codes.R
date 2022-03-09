b1fb050a77dd35667a4e6b6bd96253bb521502fe
JonteAtter <63418187+JonteAtter@users.noreply.github.com>
2022-03-09 11:42:40 +0100

    Edits in Codes

--- Codes.R
+++ Codes.R
@@ -4,6 +4,9 @@ xy<-read.csv('swetrau-scrambled.csv')
 xy$intub <- with(xy, ifelse(`pre_intubated` == 1 & is.na(xy$pre_intubated) == FALSE, 3, `ed_intubated`))
 # Förklaring: tub<- om pre inte är Na och är 1(ja), sant: konvertera till 3, Falskt: använd ed_intubated (1 är ja och 2 är nej)
 
+# JA: Även övriga variabler behöver "städas" EXV kan GCS inte vara 1-2, 15+. RR kan max 70, ISS kan ej vara 0,1,2 osv. använd unique / summary osv för att hitta fel.
+# Förslag är att göra detta tidigt i koden så att du senare kan släppa den sorteringen.
+
 install.packages("naniar")
 library(naniar)
 ab<-xy %>% replace_with_na(replace = list( "res_survival" = 999))
@@ -13,6 +16,8 @@ swetrau<-ac %>% replace_with_na(replace = list( "host_care_level" = 999))
 # Gör om blodtryck till RTS, måste klickas i ordning.
 library(dplyr)
 
+# JA: Överväg att spara dina RTS konverterade variabler direkt i en ny collumn istället för senare alternativt städa datan först.
+# Varför 350 btw?. 
 swetrau$ed_sbp_value[is.na(swetrau$ed_sbp_value)]<-350
 
 swetrau$ed_sbp_value[swetrau$ed_sbp_value>=1 & swetrau$ed_sbp_value<=49]<-1
@@ -24,6 +29,8 @@ swetrau$ed_sbp_value[swetrau$ed_sbp_value==999]<-5
 
 # Tar RTS värdet från ed, om det inte finns så använder den vanliga blodtrycket, som är grupperat enligt RTS.
 swetrau$sbp_rts<-with(swetrau, ifelse(ed_sbp_rtscat!=999 & is.na(swetrau$ed_sbp_rtscat)==FALSE,ed_sbp_rtscat,ed_sbp_value))
+
+# JA: Vad gör du här?
 
 nrow(swetrau[is.na(swetrau$inj_dominant),]+(subset(swetrau,inj_dominant=='999')))
 summary(swetrau$inj_dominant)
@@ -80,7 +87,6 @@ swetrau$pre_gcs_sum[swetrau$pre_gcs_sum==99]<-5
 swetrau$pre_gcs_sum[swetrau$pre_gcs_sum==350]<-5
 swetrau$pre_gcs_sum[swetrau$pre_gcs_sum==999]<-5
 
-
 # Skapa gcs_sum som blir vår nya gcs, 5=missing
 swetrau$gc<-with(swetrau, ifelse(intub == 3 & pre_gcs_sum !=5,pre_gcs_sum,ed_gcs_sum))
 swetrau$gcs<-with(swetrau, ifelse(intub == 1 & gc ==5,99,gc))
@@ -92,6 +98,8 @@ library(dplyr)
 rak.var<-c("Gender","intub","res_survival","host_care_level","rr_rts","sbp_rts","gcs_sum")
 cont.var<-c("ISS","dt_ed_first_ct","dt_ed_emerg_proc","pt_age_yrs")
 
+#JA: Du redovisar missing nedan genom att använda ba?
+
 ba<-select(swetrau,rak.var,cont.var)
 
 with(ba,sum(is.na(res_survival)))
@@ -102,19 +110,21 @@ ki<-swetrau[!is.na(swetrau$intub),]
 # Lägg till problemområde, Lägg till för hand funkar om koden inte gör det.
 
 library(readr)
+##### JA: Var tvungen att ändra `Problemomrade_.FMP` -> `Problemomrade_.FMP` Var det en tanke med det? ändra tillbaka om du behöver!
+
 problem_scrambled<-read.csv('problem-scrambled.csv')
 
-problem_scrambled$`Problemomrade_ FMP`[problem_scrambled$`Problemomrade_ FMP`=='ok'|problem_scrambled$`Problemomrade_ FMP`=='Ok'|problem_scrambled$`Problemomrade_ FMP`=='OK'|problem_scrambled$`Problemomrade_ FMP`=='Föredömligt handlagd']<-'No'
-problem_scrambled$`Problemomrade_ FMP`[problem_scrambled$`Problemomrade_ FMP`=='bristande rutin'|problem_scrambled$`Problemomrade_ FMP`=='Dokumentation'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Dokumetation'|problem_scrambled$`Problemomrade_ FMP`=='Handläggning'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Kommunikation'|problem_scrambled$`Problemomrade_ FMP`=='kompetens brist'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Logistik/teknik'|problem_scrambled$`Problemomrade_ FMP`=='Lång tid till DT'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Lång tid till op'|problem_scrambled$`Problemomrade_ FMP`=='Missad skada'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Neurokirurg'|problem_scrambled$`Problemomrade_ FMP`=='Tertiär survey'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Resurs'|problem_scrambled$`Problemomrade_ FMP`=='Traumakriterier/styrning'|
-                                         problem_scrambled$`Problemomrade_ FMP`=='Triage på akutmottagningen'|problem_scrambled$`Problemomrade_ FMP`=='Vårdnivå']<-'Yes'
+problem_scrambled$`Problemomrade_.FMP`[problem_scrambled$`Problemomrade_.FMP`=='ok'|problem_scrambled$`Problemomrade_.FMP`=='Ok'|problem_scrambled$`Problemomrade_.FMP`=='OK'|problem_scrambled$`Problemomrade_.FMP`=='Föredömligt handlagd']<-'No'
+problem_scrambled$`Problemomrade_.FMP`[problem_scrambled$`Problemomrade_.FMP`=='bristande rutin'|problem_scrambled$`Problemomrade_.FMP`=='Dokumentation'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Dokumetation'|problem_scrambled$`Problemomrade_.FMP`=='Handläggning'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Kommunikation'|problem_scrambled$`Problemomrade_.FMP`=='kompetens brist'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Logistik/teknik'|problem_scrambled$`Problemomrade_.FMP`=='Lång tid till DT'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Lång tid till op'|problem_scrambled$`Problemomrade_.FMP`=='Missad skada'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Neurokirurg'|problem_scrambled$`Problemomrade_.FMP`=='Tertiär survey'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Resurs'|problem_scrambled$`Problemomrade_.FMP`=='Traumakriterier/styrning'|
+                                         problem_scrambled$`Problemomrade_.FMP`=='Triage på akutmottagningen'|problem_scrambled$`Problemomrade_.FMP`=='Vårdnivå']<-'Yes'
 
-rename(problem_scrambled,probYN=`Problemomrade_ FMP`)
+rename(problem_scrambled,probYN=`Problemomrade_.FMP`)
 
 
 # Sätt ihop databaser mha id
@@ -125,17 +135,19 @@ nk<-merge(ki,problem_scrambled,by='id')
 cat.var<-c("id","probYN","Gender","res_survival","intub","host_care_level","rr_rts","sbp_rts","gcs_sum")
 cont.var<-c("ISS","dt_ed_first_ct","dt_ed_emerg_proc","pt_age_yrs")
 
-nk$probYN<-nk$`Problemomrade_ FMP`
+nk$probYN<-nk$`Problemomrade_.FMP`
 
 # Gör en en dataframe (nks) med endast våra variabler.
 library(dplyr)
 nks<-select(nk,cat.var,cont.var)
 
+# JA: Är detta för att ta bort barn från swetrau?
+
 nkk<-subset(nks,!(pt_age_yrs%in%c(1,2,3,4,5,6,7,8,9,10,11,12,13,14)))
 
 # Se alla missing values
 summary(nkk)
-
+# JA: Du kommer behöva hänvisa till antalet i texten. Extractionen kan vara exv vara Missing.RR <- sum(is.na(nkk$rr_rts)).
 aa<-na.omit(nkk)
 data.frame(table(aa$probYN))
 
@@ -144,7 +156,10 @@ ae<-aa
 
 # Logistic regression
 
+
   # Sätt alla cat.var som factor variables
+
+#JA: Tror det går att göra as.factor på delar av dataset om du villa slippa köra collumn för collumn. men fungerar såklart. 
 str(ae)
 ae$probYN<-as.factor(ae$probYN)
 ae$Gender<-as.factor(ae$Gender)
@@ -158,7 +173,6 @@ ae$gcs_sum<-as.factor(ae$gcs_sum)
 
 # Rita ut alla grafer
 pairs(ae,col=ae$probYN)
-
 
 # Logistic regression på variabler för sig
 sex<-glm(probYN~Gender,data = ae,family = 'binomial')
@@ -282,6 +296,10 @@ table1(~ Gender + pt_age_yrs + res_survival + host_care_level + intub + ISS + rr
 
 install.packages("PRISMAstatement")
 library(PRISMAstatement)
+
+#JA: Finns det en anledning till atu valt att lägga intubations status som en separat flik innan och utanför övriga missing values?
+# Förslag är att intubation ingår i missing values och att du även presenterar en tabell med de olika variablernas NA. 
+# Vi får klura på exakt hur vi presenterar att en patient kan ha flera missing: Prel med en kort deskriptiv text kring det och sedan bara total missing. 
 
 flow_exclusions(
   incl_counts = c(nrow(xy),nrow(ki), nrow(nks), nrow(nkk),nrow(aa)),
